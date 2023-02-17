618cccf8d5762469f910c3bac801ff3b218af761
peter <109552816+petermvgz@users.noreply.github.com>
2023-02-16 17:24:00 -0800

    Initial Commit

---
+++ assigningGrade_App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  assigningGrade_App
+//
+//  Created by Peter Deyi on 2/16/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ assigningGrade_App/assigningGrade_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  assigningGrade_AppApp.swift
+//  assigningGrade_App
+//
+//  Created by Peter Deyi on 2/16/23.
+//
+
+import SwiftUI
+
+@main
+struct assigningGrade_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
